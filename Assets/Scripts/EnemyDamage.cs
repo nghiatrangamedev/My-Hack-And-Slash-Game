84177e1db60d943feef1fecb29378f1b8ba0ca59
nghiatran98 <nghiatrangamedev@gmail.com>
2022-07-20 21:46:15 +0700

    Update
    Complete a simple map

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -23,7 +23,7 @@ public class EnemyDamage : MonoBehaviour
 
     IEnumerator WaitForBeDestroy()
     {
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(0.2f);
         Destroy(gameObject);
     }
 
